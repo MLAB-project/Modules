9b56f5bb1ae44135e3d83f658c15969f41757586
KAKL <kakl@users.noreply.github.com>
2011-06-24 09:09:40 +0000

    Vycistena verze.

---
+++ ARM/STM32F10xRxT/SW/RS232_bootloader/Src/BIN/Release/test2.cmd
@@ -0,0 +1,2 @@
+rem STMFlashLoader.exe -c --pn 20 --br 115200 --to 1000 -Auto -i STM32_Connectivity-line_256K  -e --all -d --fn LED_Blink.hex --v
+STMFlashLoader.exe -c --pn 20 --br 115200 --to 1000 -Auto -i STM32_Connectivity-line_256K -e --all -o -d --fn test2.hex
