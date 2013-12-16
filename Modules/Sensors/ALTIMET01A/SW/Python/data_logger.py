5c83e62320bc0fd2b40b9084e4b85114b2717546
kaklik <kaklik@f55caa52-0715-0410-8694-db4083128028>
2013-12-16 10:21:47 +0000

    pokus se ctenim seriove linky

--- Modules/Sensors/ALTIMET01A/SW/Python/data_logger.py
+++ Modules/Sensors/ALTIMET01A/SW/Python/data_logger.py
@@ -72,12 +72,12 @@ time.sleep(0.5)
 
 #### Data Logging ###################################################
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-print ser.name
-ser.write("hello")
+#ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+#print ser.name
+#ser.write("hello")
 #http://pyserial.sourceforge.net/shortintro.html
-line = ser.readline()   # read a '\n' terminated line
-ser.close()
+#line = ser.readline()   # read a '\n' terminated line
+#ser.close()
 
 try:
     with open("temperature.log", "a") as f:
