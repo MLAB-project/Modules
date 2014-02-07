9bbf44bcdc7689af7883ad1b3eb3fcf6a7b42796
Jakub Kákona <kaklik@users.noreply.github.com>
2014-02-07 14:43:24 +0000

    pridani dokumentace

--- Sensors/ALTIMET01A/SW/Python/dpi145_test.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-print ser.name
-ser.write("hello")
-http://pyserial.sourceforge.net/shortintro.html
-line = ser.readline()   # read a '\n' terminated line
-ser.close()
-
