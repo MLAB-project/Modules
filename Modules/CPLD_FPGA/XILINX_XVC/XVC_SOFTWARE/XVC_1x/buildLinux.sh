6733225ef323cd19eb4ba9da7a1a51f2594962a2
kaklik <kaklik@f55caa52-0715-0410-8694-db4083128028>
2016-05-18 09:19:14 +0000

    uprava modelu.

--- Modules/CPLD_FPGA/XILINX_XVC/XVC_SOFTWARE/XVC_1x/buildLinux.sh
+++ Modules/CPLD_FPGA/XILINX_XVC/XVC_SOFTWARE/XVC_1x/buildLinux.sh
@@ -5,5 +5,5 @@
 #     sudo apt-get install g++-multilib
 
 # Just compile and link the target executable:
-gcc mlab_xvcd*.cpp lib_linux/i386/libftd2xx.a   -m32 -lstdc++ -ldl -lpthread -lrt -Os -o mlab_xvcd_i386
-gcc mlab_xvcd*.cpp lib_linux/x86_64/libftd2xx.a -m64 -lstdc++ -ldl -lpthread -lrt -Os -o mlab_xvcd_x86_64
+#gcc mlab_xvcd*.cpp lib_linux/i386/libftd2xx.a   -m32 -lstdc++ -ldl -lpthread -lrt -Os -o mlab_xvcd_i386
+gcc mlab_xvcd*.cpp lib_linux/x86_64/libftd2xx.a -m64 -lstdc++ -ldl -lrt -Os -o mlab_xvcd_x86_64
