ec20cb3de41ebbfe9d4c29fac27216dd5c492174
Jakub Kákona <kaklik@users.noreply.github.com>
2016-08-22 13:16:59 +0000

    prodlouzena doba odezvy na vypadek a pridano potvrzeni zmeny ve dvou bodech.

--- PowerSupply/SUPERCAP01A/SW/SUPERCAP.py
+++ PowerSupply/SUPERCAP01A/SW/SUPERCAP.py
@@ -13,10 +13,10 @@ oldstav = gpio.digitalRead(88)
 while True:
     stav = gpio.digitalRead(88)
 
-    if stav != oldstav and stav == 0:
+    if oldstav == 0 and stav == 0:
       subprocess.call("shutdown -P 1", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
       oldstav = stav
 
-    time.sleep(1)
+    time.sleep(2)
 
 
