719a4e77e1adc01d207f4f2a0e4177c0d2e6e53e
Jakub Kákona <kaklik@users.noreply.github.com>
2016-09-08 11:30:21 +0000

    zmena signalizacniho pinu vypadku napajeni.

--- PowerSupply/SUPERCAP01A/SW/SUPERCAP.py
+++ PowerSupply/SUPERCAP01A/SW/SUPERCAP.py
@@ -3,21 +3,24 @@ import wiringpi2 as gpio
 import time
 import subprocess
 
+power_sense_pin = 83
+
 gpio.wiringPiSetupGpio()
-gpio.pinMode(88, 0)
-gpio.pullUpDnControl(88, 0)
+gpio.pinMode(power_sense_pin, 0)
+gpio.pullUpDnControl(power_sense_pin, 0)
 
+time.sleep(2)
 
-oldstav = gpio.digitalRead(88)
+oldstav = gpio.digitalRead(power_sense_pin)
 
 while True:
-    stav = gpio.digitalRead(88)
+    stav = gpio.digitalRead(power_sense_pin)
 
     if oldstav == 0 and stav == 0:
       subprocess.call("shutdown -P 0", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     else:
 	print "power is OK."
-    
+
     oldstav = stav
 
     time.sleep(2)
