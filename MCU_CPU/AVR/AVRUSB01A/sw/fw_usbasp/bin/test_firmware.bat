d197de4a0d7a2be085eb839b77cdab162d6cf634
Roman Dvořák <romandvorak@mlab.cz>
2023-06-20 19:11:11 +0200

    Removing AVRUSB01A modules splitted to separate repositories.

--- MCU_CPU/AVR/AVRUSB01A/sw/fw_usbasp/bin/test_firmware.bat
+++
@@ -1,8 +0,0 @@
-rem Test if programmer is working
-rem
-rem Fuses:
-rem   CPU        EFUSE   HFUSE   LFUSE
-rem   ATmega8            C9      9F
-rem   ATmega88   F9      DE      D7
-rem
-avrdude -c usbasp -p atmega88 -F
