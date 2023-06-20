ce9c96cfade3745d69f915a71cdd5b79c4cd421e
Roman Dvořák <romandvorak@mlab.cz>
2023-06-20 19:09:54 +0200

    Removing ATprogISPUSB02A modules splitted to separate repositories.

--- MCU_CPU/AVR/ATprogISPUSB02A/sw/HEX_STK500V2/README.txt
+++
@@ -1,3 +0,0 @@
-Flashovaci skripty  pro nahrani firmwaru do nove sestaveneho programatoru. 
-
-skript "flash.sh" vyuziva kk nahravani firmavaru jednoduchy zakladni DAPA programator sestaveny z paralelniho portu. Skript "flash_by_stk500v2.sh" nahraje firmware pres programator STK500V2 tedy stejny, jako ten do ktereho se firmvare nahrava. 
--- MCU_CPU/AVR/ATprogISPUSB02A/sw/HEX_STK500V2/flash_by_stk500v2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ATprogISPUSB02A fash script
-# Uses STK500v2 Programmer or another functioning ATprogISPUSB02A module.
-#echo "WARNING: Enables external clock. Make sure a crystal is connected to the ATmega8535! \
-#Connect a programmer to the ATprogISPUSB02A and set a 'ISP SELECTION' jumper (J4) to 'internal' (1-2)"
-avrdude -p atmega8535 -P /dev/ttyUSB0 -c stk500v2 -B 50 -v -U lfuse:w:0xff:m -U hfuse:w:0xdb:m
-avrdude -p atmega8535 -P /dev/ttyUSB0 -c stk500v2 -B 50 -U flash:w:stk500.hex:a
-echo "If none errors above - Please set 'ISP SELECTION' jumper (J4) to 'normal' (2-3) position."
